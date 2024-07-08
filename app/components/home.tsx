1ef201b30e0783df8432cb36e0c9a908f881931d
GianniCrux <gianni.disti@gmail.com>
2024-07-08 15:49:27 +0200

    first commit

---
+++ app/components/home.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+const skills = [
+    { name: 'Skill 1', image: '/placeholder.png' },
+    { name: 'Skill 2', image: '/placeholder.png' },
+    { name: 'Skill 3', image: '/placeholder.png' },
+    // Add more skills as needed
+  ]
+  
+
+export const HomePage = () => {
+
+
+
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
