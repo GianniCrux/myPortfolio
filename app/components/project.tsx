d4a85cc466a5b4e128f52934b4577c932c5146a2
GianniCrux <gianni.disti@gmail.com>
2024-07-12 16:24:12 +0200

    fixed hovering and general layout for projects

--- app/components/project.tsx
+++ app/components/project.tsx
@@ -20,18 +20,18 @@ interface ProjectProps {
               src={imageUrl} 
               alt={`${title} screenshot`} 
               fill
-              className="object-cover" 
+              className="object-contain" 
             />
           </div>
         </a>
         <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          <a href={link} target="_blank" rel="noopener noreferrer" className="block p-4 w-full h-full">
-          <div className="relative w-full aspect-video">           
+          <a href={link} target="_blank" rel="noopener noreferrer" className="block p-4 w-full h-full items-center justify-center">
+          <div className="relative w-full h-full ">           
               <Image 
                 src={previewImage} 
                 alt={`${title} preview`} 
                 fill
-                className="object-cover" 
+                className="object-contain" 
               />
           </div>
           </a>
