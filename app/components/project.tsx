44336b1979eec5ff34ef53bf7bb3014e8ecc1f42
GianniCrux <gianni.disti@gmail.com>
2024-07-12 16:07:34 +0200

    changed proejcts widths and heights

--- app/components/project.tsx
+++ app/components/project.tsx
@@ -11,27 +11,29 @@ interface ProjectProps {
   
   const Project: React.FC<ProjectProps> = ({ title, description, imageUrl, link, previewImage }) => {
     return (
-      <div data-aos="flip-right" className="relative group overflow-hidden border border-gray-300 dark:border-gray-700 rounded-lg shadow-md">
+      <div data-aos="flip-right" className="relative group overflow-hidden border border-gray-300 dark:border-gray-700 rounded-lg shadow-md w-full max-w-xs">
         <a href={link} target="_blank" rel="noopener noreferrer" className="block p-4">
-          <h3 className="text-xl font-bold mb-2">{title}</h3>
-          <p className="text-gray-700 dark:text-gray-300 mb-4">{description}</p>
-          <Image 
-            src={imageUrl} 
-            alt={`${title} screenshot`} 
-            width={600} 
-            height={400}
-            className="w-full h-auto" 
-        />
+          <h3 className="text-lg font-bold mb-2">{title}</h3>
+          <p className="text-gray-700 text-sm dark:text-gray-300 mb-4">{description}</p>
+          <div className="relative w-full aspect-video">
+            <Image 
+              src={imageUrl} 
+              alt={`${title} screenshot`} 
+              fill
+              className="object-cover" 
+            />
+          </div>
         </a>
         <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          <a href={link} target="_blank" rel="noopener noreferrer" className="block p-4">
-            <Image 
-              src={previewImage} 
-              alt={`${title} preview`} 
-              width={600} 
-              height={400}
-              className="w-full h-auto" 
-            />
+          <a href={link} target="_blank" rel="noopener noreferrer" className="block p-4 w-full h-full">
+          <div className="relative w-full aspect-video">           
+              <Image 
+                src={previewImage} 
+                alt={`${title} preview`} 
+                fill
+                className="object-cover" 
+              />
+          </div>
           </a>
         </div>
       </div>
--- app/components/random-lines.tsx
+++ app/components/random-lines.tsx
@@ -3,12 +3,20 @@
 import React from 'react'
 
 function StraightLines() {
-  const lines = [
-    { startX: 5, startY: 5, length: 10, angle: 45, curved: false },
-    { startX: 95, startY: 5, length: 10, angle: 135, curved: false },
-    { startX: 5, startY: 95, length: 10, angle: 315, curved: false },
-    { startX: 95, startY: 95, length: 10, angle: 225, curved: false },
-  ]
+  const lines = 
+/*   [
+    { startX: 5, startY: 5, length: 40, angle: 45, curved: false },
+    { startX: 95, startY: 5, length: 40, angle: 135, curved: false },
+    { startX: 5, startY: 95, length: 40, angle: 315, curved: false },
+    { startX: 95, startY: 95, length: 40, angle: 225, curved: false },
+    /* { startX: 50, startY: 50, length: 50, angle: 0, curved: false },  //center line
+  ] */
+    [
+      { startX: 5, startY: 5, length: 40, angle: 45, curved: false },
+      { startX: 95, startY: 5, length: 40, angle: 135, curved: false },
+      { startX: 5, startY: 95, length: 40, angle: 315, curved: false },
+      { startX: 95, startY: 95, length: 40, angle: 225, curved: false },
+    ]
 
   return (
     <div className="fixed inset-0 z-0 opacity-50 pointer-events-none">
@@ -32,17 +40,17 @@ function StraightLines() {
 
 function RoundedLines() {
   const lines = [
-    // Top-left 
-    "M5,5 C10,10 5,15 10,20 S15,25 10,30",
-    
-    // Top-right 
-    "M95,5 C90,10 95,15 90,20 S85,25 90,30",
-    
-    // Bottom-left 
-    "M5,95 C10,90 5,85 10,80 S15,75 10,70",
-    
-    // Bottom-right 
-    "M95,95 C90,90 95,85 90,80 S85,75 90,70"
+  // Top-left corner
+  "M5,5 C20,20 10,30 30,25 S40,5 35,15",
+  
+  // Top-right corner
+  "M95,5 C80,20 90,30 70,25 S60,5 65,15",
+  
+  // Bottom-left corner
+  "M5,95 C20,80 10,70 30,75 S40,95 35,85",
+  
+  // Bottom-right corner
+  "M95,95 C80,80 90,70 70,75 S60,95 65,85"
   ]
 
   return (
